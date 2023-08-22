fb88070458cb4be9d7125dc7dbff23a44ffa86b4
Dan Connolly <dckc@madmode.com>
2023-08-22 11:47:23 -0500

    feat: script to deploy bundles only if necessary

---
+++ deploy-bundles.sh
@@ -0,0 +1,21 @@
+NODE=https://ollinet.rpc.agoric.net:443
+WALLET=gov1
+CHAIN_ID=agoricollinet-dev-1
+
+install_bundle() {
+  bfile=$1
+  bid=$(echo $bfile | sed -e 's,^bundles/,,' | sed -e 's,.json$,,')
+  found=$(agd --node=$NODE query vstorage data swingStore.bundle.$bid -o json | jq -r .value)
+  if [[ -n "$found" ]]; then
+    echo "bundle $bid already installed"
+    return
+  fi
+  agd tx swingset install-bundle "@$1" \
+    --node $NODE \
+    --from $WALLET --keyring-backend=test --gas=auto \
+    --chain-id=$CHAIN_ID -bblock --yes -o json
+}
+
+for b in bundles/b1-*; do
+  install_bundle $b
+done
